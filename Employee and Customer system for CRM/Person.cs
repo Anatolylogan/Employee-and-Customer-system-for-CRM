acb37ff9f1f5650667da62ad8158806f6c0a5431
Anatolylogan <homenkotolya2@gmail.com>
2024-10-29 22:38:59 +0700

    Добавьте файлы проекта.

---
+++ Employee and Customer system for CRM/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_and_Customer_system_for_CRM
+{
+    abstract class Person
+    {
+        public abstract string Name { get; set; }
+        public abstract int Age { get; set; }
+
+        public abstract string GetDetails();
+        public abstract string CalculateDiscount();
+        public override string ToString()
+        {
+            return $"Person:{Name} Age:{Age} ";
+        }
+    }
+}
