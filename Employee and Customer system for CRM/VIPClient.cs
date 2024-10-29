6f8fbb45d8e74ca8558fa5eada2a56f3a8e15361
Anatolylogan <homenkotolya2@gmail.com>
2024-10-30 00:21:30 +0700

    2.Создал класс VIPClient -Добавил виртуальное свойство VIPLevel, Которое можно скрывать в наследуемых классах -Добавил скрытый метод GetDetails, дабвив с троку информации парметр VIPLevel -Что бы  увидеть резултат метода GetDitails Из VIPCLient , переопределил его в Program

---
+++ Employee and Customer system for CRM/VIPClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_and_Customer_system_for_CRM
+{
+    public class VIPClient : Client
+    {
+        public virtual string VIPLevel { get; set; } = "Standard";
+        protected new string GetDetails()
+        {
+            return $"VIPClient: Person {Name} Age {Age} PurchaseAmount {PurchaseAmount} VIPLevel {VIPLevel}";
+        }
+        public string GetVIPDetails()
+        {
+            return GetDetails();
+        }
+    }
+}
