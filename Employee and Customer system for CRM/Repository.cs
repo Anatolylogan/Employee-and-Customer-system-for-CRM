bbb3124a1d1272ac29e497f1e30f076fb60770e5
Anatolylogan <homenkotolya2@gmail.com>
2024-10-30 00:35:48 +0700

    2. Создал обобщенный класс Repository<T>. - Ограничил тип Т - Добавил метод Add(T item) для добавления элемента -Добавил метод GetAll() для получения всех элементов

---
+++ Employee and Customer system for CRM/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_and_Customer_system_for_CRM
+{
+    public class Repository<T> where T : Person
+    {
+        private List<T> items = new List<T>();
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        public List<T> GetAll()
+        {
+            return new List<T>(items);
+        }
+    }
+}
